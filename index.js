1b1652dec6d3daef45eb292999bd688015b7b1f2
Jon New <jonnew@gmail.com>
2016-01-30 22:56:25 -0800

    Refactoring routes to take advantage of flickr library. Other minor tweaks.

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
-var nconf = require("nconf"),
-	express = require('express'),
-	app = express();
+"use strict";
 
+var nconf = require("nconf");
 nconf.env().file({file: ".env"});
 
-var port = nconf.get("PORT");
+var express = require('express'),
+	flickr = require("./flickr.js"),
+	fs = require("fs"),
+	app = express(),
+	port = nconf.get("PORT");
 
-app.use('/', express.static(__dirname + "/html"));
+flickr.init(function(err){
+	if(err){
+		console.error("Error retrieving flickr api.", error);
+		throw err;
+	}
+	else{
+		console.log("Flickr apis are ready");
+	}
+	
 
-//javascript files
-app.use("/js", express.static(__dirname + "/js"));
+	app.get("/getPhotos.php", function(req, res){
+		var page = req.query.page || 1,
+			tag = req.query.tag;
+		flickr.getPhotos(page, tag, function(err, results){
+			res.send(results);
+		});
+  	});
 
-//css files
-app.use("/css", express.static(__dirname + "/css"));
+	app.get('/', function(req, res){
+		fs.readFile("./html/index.html", "utf8", function(fileErr, fileContents){
+			if(err){
+				console.error("Error reading file", fileErr);
+				res.status(404);
+			}
+			else{
+				flickr.getPhotosDefault(function(photoErr, results){
+					if(err){
+						console.error("Error fetching photos", photoErr);
+						results = "{}";
+					}
 
-//static images
-app.use("/img", express.static(__dirname + "/img"));
+					//lame template, but it's the only place I need it.
+					fileContents = fileContents.split("//STARTING_DATA_HERE").join(JSON.stringify(results));
+					res.send(fileContents);
+				});
+				
+			}
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});	
+		});
+	});
+	app.use('/', express.static(__dirname + "/html"));
+
+	//javascript files
+	app.use("/js", express.static(__dirname + "/js"));
+
+	//css files
+	app.use("/css", express.static(__dirname + "/css"));
+
+	//static images
+	app.use("/img", express.static(__dirname + "/img"));
+
+	app.listen(port, function() {
+	  console.log("Listening on " + port);
+	});	
+
+});
